692f99cf7638675ef88fa5f5728863d3ba240640
MurdocAV <azaan.virk1@gmail.com>
2019-08-07 18:18:12 +1200

    hello

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import "../App.css";
-import Nav from "./Nav";
-import Index from "./Index.jsx";
+import React from 'react'
+import Center from './Center'
+import ImageDrawer from './ImageDrawer'
+import AppBar from './AppBar'
+import {HashRouter} from 'react-router-dom'
+
+//TODO: Test out Material U.I.; 
+//  1. Use Material's grid to make layered mobile first Responsive U.I.
+//  1.1. Allow user to see the mobile view on desktop
+//  2. Use 'Drawer' component to switch between different components(pages)
+//  S1. Optional (Strech) --> Use breadcrumbs to do complex navigations.
+//  S2. (Strech) --> Advanced Grid list with titlebars (images of worked projects)
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: <Index />,
-      active: "Index"
-    };
-    this.changePage = this.changePage.bind(this);
-  }
-
-  changePage(pageHtml) {
-    this.setState({
-      page: pageHtml,
-      active: pageHtml.type.name
-    });
-  }
-
-  render() {
+  render () {
     return (
-      <Router>
-        <div className="App">
-          <Nav changePage={this.changePage} active={this.state.active} />
-          {this.state.page}
-        </div>
-      </Router>
-    );
+      <HashRouter>
+        {/* <AppBar className="appBar"></AppBar> */}
+        <Center></Center>
+        {/* <div className='imageDrawer'><ImageDrawer /></div> */}
+      </HashRouter>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/tileData.js
@@ -0,0 +1,21 @@
+const tileData = [
+  {
+    img: '/images/LukeMitchells.png',
+    title: 'Luke Mitchells',
+    author: 'MurdocAV',
+    cols: 2,
+    featured: true
+  },
+  {
+    img: '/images/DrinksRecomendation.png',
+    title: 'Drink Recomendation',
+    author: 'MurdocAV',
+  },
+  {
+    img: '/images/PokeName.png',
+    title: 'PokeName',
+    author: 'MurdocAV',
+  }
+]
+
+export default tileData
