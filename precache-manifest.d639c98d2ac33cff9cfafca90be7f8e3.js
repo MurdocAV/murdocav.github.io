def26f87e681254db61fcfcf83e0f0c48f9b398e
MurdocAV <azaan.virk1@gmail.com>
2019-11-13 13:52:05 +1300

    Updates

---
+++ precache-manifest.d639c98d2ac33cff9cfafca90be7f8e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdd621471a78587cc925323322ebc3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc6fd6bff93564fb0bd1",
+    "url": "/static/js/2.274bf4dc.chunk.js"
+  },
+  {
+    "revision": "6aee76e46fa26b80f47e",
+    "url": "/static/js/main.fab0fc3c.chunk.js"
+  },
+  {
+    "revision": "28fad3655b10e5ff2dec",
+    "url": "/static/js/runtime-main.57f18d8e.js"
+  }
+]);
