7baae0d0f68cdf2aba990d2021a6adbb5d03afba
MurdocAV <azaan.virk1@gmail.com>
2020-02-14 14:04:15 +1300

    pre deployment commit 12/02/20

--- src/components/Cards/HireCard.jsx
+++ src/components/Cards/HireCard.jsx
@@ -41,7 +41,7 @@ export default function HireCard(props) {
         <Typography className={classes.pos} color="textSecondary">
         </Typography>
         <Typography variant="body2" component="p">
-          All the best with your adventures.
+          Don't hesitate, let's talk and start to create.
         </Typography>
       </CardContent>
       <CardActions>
